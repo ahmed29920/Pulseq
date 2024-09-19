b32d00dfabed22d44a60340d20c01f83f18c3b0d
ahmed29920 <aa4063574@gmail.com>
2024-09-19 18:35:48 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -17,3 +17,53 @@ $(document).on('click', '.edit-btn', function() {
   $('.form-select').prop('disabled',false);
   $('.save-btn').removeClass('d-none');
 });
+
+$(document).on('click','.history-btn',function(){
+  $('.start-quests-btn').removeClass('active-history-div');
+  $('.reported-quests-btn').removeClass('active-history-div')
+  $(this).addClass('active-history-div')
+  $('.history-table').removeClass('d-none');
+  $('.clearfix').removeClass('d-none');
+  $('.stared-div').addClass('d-none');
+  $('.reported-div').addClass('d-none');
+
+})
+$(document).on('click','.start-quests-btn',function(){
+  $('.history-btn').removeClass('active-history-div');
+  $('.reported-quests-btn').removeClass('active-history-div')
+  $(this).addClass('active-history-div')
+  $('.history-table').addClass('d-none');
+  $('.stared-div').removeClass('d-none');
+  $('.clearfix').addClass('d-none');  
+  $('.reported-div').addClass('d-none');
+
+})
+$(document).on('click','.reported-quests-btn',function(){
+  $('.history-btn').removeClass('active-history-div');
+  $('.start-quests-btn').removeClass('active-history-div')
+  $(this).addClass('active-history-div')
+  $('.history-table').addClass('d-none');
+  $('.clearfix').addClass('d-none');
+  $('.stared-div').addClass('d-none');
+  $('.reported-div').removeClass('d-none');
+
+
+})
+$(document).on('click','.star-div',function(){
+  if($(this).hasClass('starred')){
+    $(this).removeClass('starred')
+    $(this).empty().append(`<i class="fa-regular fa-star fa-lg text-warning"></i>`)
+
+  }else{
+    $(this).addClass('starred')
+    $(this).empty().append(`<i class="fa-solid fa-star fa-lg text-warning"></i>`)
+
+  }
+})
+$(document).on('click','.option',function(){
+  $(this).addClass('selected-option')
+  $(this).siblings().removeClass('selected-option')
+})
+$(document).on('click','.remove-notification',function(){
+  $(this).parent().parent().remove()
+})
